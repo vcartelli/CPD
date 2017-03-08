6e9e151b17dd4ccf1e28705c11674df75096dd73
Vincenzo Cartelli <vincenzo.cartelli@gmail.com>
2017-03-08 23:18:48 +0100

    Delete keystore-example.sh

--- keystore-example.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# replace <MY_KEYSTORE_FILENAME> and <MY_KEYSTORE_PASSWORD> with your own keystore filename and password
-echo "writing keystore settings to config.json..."
-sed -i '' -e 's/$KEYSTORE_FILENAME/<MY_KEYSTORE_FILENAME>/g' target/deploy-bundle/conf/config.json
-sed -i '' -e 's/$KEYSTORE_PASSWORD/<MY_KEYSTORE_PASSWORD>/g' target/deploy-bundle/conf/config.json
