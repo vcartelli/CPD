68398ede52269479b591b26739f011d1bd91a537
Vincenzo Cartelli <vincenzo.cartelli@gmail.com>
2017-03-08 23:18:35 +0100

    Delete oauth-example.sh

--- oauth-example.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# replace <MY_OAUTH2_CLIENT_ID> and <MY_OAUTH2_CLIENT_SECRET> with your own oauth2 keys
-echo "writing oauth2 keys to config.json..."
-sed -i '' -e 's/$OAUTH2_CLIENT_ID/<MY_OAUTH2_CLIENT_ID>/g' target/deploy-bundle/conf/config.json
-sed -i '' -e 's/$OAUTH2_CLIENT_SECRET/<MY_OAUTH2_CLIENT_SECRET>/g' target/deploy-bundle/conf/config.json
