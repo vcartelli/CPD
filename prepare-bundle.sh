a40703bb226646463c44427596931fcc1eb1935b
Vincenzo Cartelli <vince@MacBook-Pro-di-Vince.local>
2017-03-09 16:20:24 +0100

    release beta-10

---
+++ prepare-bundle.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+PROFILE=$1
+if [ -z "$PROFILE" ]; then
+  PROFILE=production
+fi
+if [ "$PROFILE" != "develop" ] && [ "$PROFILE" != "production" ]; then
+  echo "unknown profile '$PROFILE', it should be 'develop' or 'production'"
+  exit 1
+fi;
+cd "$(dirname "$0")"
+echo "starting $PROFILE compilation..."
+mvn clean package -P $PROFILE
+echo "copying target jar to deploy-bundle..."
+cp target/cpd-server-1.0-SNAPSHOT-fat.jar target/deploy-bundle/cpd-server.jar
+chmod 754 target/deploy-bundle/*.sh
