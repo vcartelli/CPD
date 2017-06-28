7ba04af273fb39b02a64aa2fa346b17f3ab0e197
Vincenzo Cartelli <vince@MacBook-Pro-di-Vince.local>
2017-06-28 17:11:40 +0200

    final-pre-evaluation

--- db-dump.sh
+++ db-dump.sh
@@ -1,2 +1,15 @@
 #!/usr/bin/env bash
-mongodump --db cpd --gzip
+PILOT=$1
+if ! [[ "$PILOT" =~ ^(trento|galicia|sheffield|all)$ ]]
+then
+  echo "please provide a pilot ID (trento|galicia|sheffield)"
+  exit 1
+fi
+mongodump --out "dump/$PILOT" --db cpd --gzip
+SRC=$2
+if [ $PILOT == "all" ] && [ $SRC == "src" ]
+then
+  mongoexport --db cpd --collection schema --jsonArray --pretty --out src/main/deploy-bundle/data/db/schema.json
+  mongoexport --db cpd --collection model --jsonArray --pretty --out src/main/deploy-bundle/data/db/model.json
+  mongoexport --db cpd --collection diagram --jsonArray --pretty --out src/main/deploy-bundle/data/db/diagram.json
+fi
--- db-restore.sh
+++ db-restore.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
-mongorestore --nsInclude 'cpd.*' --drop --gzip
+PILOT=$1
+if ! [[ "$PILOT" =~ ^(trento|galicia|sheffield|all)$ ]]
+then
+  echo "please provide a pilot ID (trento|galicia|sheffield)"
+  exit 1
+fi
+mongorestore --dir "dump/$PILOT" --nsInclude 'cpd.*' --drop --gzip
