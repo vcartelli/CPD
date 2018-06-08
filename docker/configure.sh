9f8b88fe35e7517abdaeaf3f8755c11f9ec2599f
Giuseppe Di Modica <gdimodica@gmail.com>
2018-06-08 14:57:33 +0200

    Update configure.sh

--- docker/configure.sh
+++ docker/configure.sh
@@ -87,8 +87,10 @@ foundoauth2providers=false
 for input in $(grep "^[^#\!].*$" $PRODUCTIONFILE);
 do 
 #cat "$input" | xxd -p -r > "${input%'.hex'}"; rm "$input"; 
+set -f
 IFS='=' read -r key value <<< "$input"
 array=($key $value)
+set +f
 echo "read ${array[0]}=${array[1]}"
 #if  [ ${array[0]} != "cpd.server.pub.scheme" ] || [ ${array[0]} != "cpd.oauth2.origin" ] ; then
 if ! [[ ${array[0]} =~ ^cpd\.(server\.scheme|oauth2\.origin|oauth2\.providers)$ ]] ; then  
