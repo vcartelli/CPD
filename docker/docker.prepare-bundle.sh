07c4fc8328e084bf9b7bd969b9b50bc483108b2c
gdimodica <fidelIO66>
2018-09-24 03:09:42 +0200

    fixes....

--- docker/docker.prepare-bundle.sh
+++ docker/docker.prepare-bundle.sh
@@ -5,5 +5,5 @@ echo "starting docker compilation..."
 #mvn -DskipTests -Drelax -gs /usr/share/maven/conf/settings-docker.xml package -P $PROFILE
 mvn clean package -P docker
 echo "copying target jar to deploy-bundle..."
-cp target/cpd-server-1.4-SNAPSHOT-fat.jar target/deploy-bundle/cpd-server.jar
+cp target/cpd-server-1.5-SNAPSHOT-fat.jar target/deploy-bundle/cpd-server.jar
 chmod 754 target/deploy-bundle/*.sh
